30049c4fa98bccec4e8602c805b088234c169214
Vincent Boutour <bob@vibioh.fr>
2023-11-02 15:55:35 +0100

    feat: Switching to multi namespace without wildcard
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/job/job.go
+++ pkg/job/job.go
@@ -6,12 +6,16 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/ViBiOh/flags"
+	"github.com/ViBiOh/httputils/v4/pkg/concurrent"
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -30,7 +34,7 @@ type Config struct {
 func Flags(fs *flag.FlagSet, prefix string, overrides ...flags.Override) *Config {
 	var config Config
 
-	flags.New("Namespace", "Namespace to watch (blank for all)").Prefix(prefix).DocPrefix("job").StringVar(fs, &config.Namespace, "default", overrides)
+	flags.New("Namespace", "Namespace to watch (blank for all, comma separated otherwise)").Prefix(prefix).DocPrefix("job").StringVar(fs, &config.Namespace, "default", overrides)
 	flags.New("Label", "Label selector for jobs").Prefix(prefix).DocPrefix("job").StringVar(fs, &config.Label, "k8s-cleaner=true", overrides)
 	flags.New("Duration", "TTL Duration after succeeded").Prefix(prefix).DocPrefix("job").DurationVar(fs, &config.Duration, time.Minute*2, overrides)
 
@@ -38,11 +42,11 @@ func Flags(fs *flag.FlagSet, prefix string, overrides ...flags.Override) *Config
 }
 
 type Service struct {
-	k8s       *kubernetes.Clientset
-	done      chan struct{}
-	namespace string
-	label     string
-	payload   []byte
+	k8s        *kubernetes.Clientset
+	done       chan struct{}
+	namespaces []string
+	label      string
+	payload    []byte
 }
 
 func New(config *Config, k8s *kubernetes.Clientset) Service {
@@ -56,31 +60,45 @@ func New(config *Config, k8s *kubernetes.Clientset) Service {
 	}
 
 	return Service{
-		k8s:       k8s,
-		namespace: config.Namespace,
-		label:     config.Label,
-		payload:   payload,
-		done:      make(chan struct{}),
+		k8s:        k8s,
+		namespaces: strings.Split(config.Namespace, "'"),
+		label:      config.Label,
+		payload:    payload,
+		done:       make(chan struct{}),
 	}
 }
 
-func (a Service) Done() <-chan struct{} {
-	return a.done
+func (s Service) Done() <-chan struct{} {
+	return s.done
 }
 
-func (a Service) Start(ctx context.Context) {
-	defer close(a.done)
+func (s Service) Start(ctx context.Context) {
+	defer close(s.done)
+
+	var wg sync.WaitGroup
+
+	for _, namespace := range s.namespaces {
+		wg.Add(1)
+
+		go s.watchJobs(ctx, &wg, namespace)
+	}
+
+	wg.Wait()
+}
+
+func (s Service) watchJobs(ctx context.Context, wg *sync.WaitGroup, namespace string) {
+	defer wg.Done()
 
 	for {
-		if a.watchJobs(ctx) {
+		if s.watchNamespace(ctx, namespace) {
 			return
 		}
 	}
 }
 
-func (a Service) watchJobs(ctx context.Context) bool {
-	watcher, err := a.k8s.BatchV1().Jobs(a.namespace).Watch(ctx, v1.ListOptions{
-		LabelSelector: a.label,
+func (s Service) watchNamespace(ctx context.Context, namespace string) bool {
+	watcher, err := s.k8s.BatchV1().Jobs(namespace).Watch(ctx, v1.ListOptions{
+		LabelSelector: s.label,
 		Watch:         true,
 	})
 	if err != nil {
@@ -88,39 +106,33 @@ func (a Service) watchJobs(ctx context.Context) bool {
 		os.Exit(1)
 	}
 
-	slog.Info("Listening jobs", "namespace", a.namespace, "label", a.label)
+	slog.Info("Listening jobs", "namespace", namespace, "label", s.label)
 
-	defer watcher.Stop()
+	var done bool
 
-	results := watcher.ResultChan()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return true
-		case event, ok := <-results:
-			if !ok {
-				return false
-			}
-
-			job, ok := event.Object.(*batchv1.Job)
-			if !ok {
-				continue
-			}
-
-			if job.Spec.TTLSecondsAfterFinished != nil && *job.Spec.TTLSecondsAfterFinished != 0 {
-				continue
-			}
-
-			if job.Status.Succeeded != 1 {
-				continue
-			}
-
-			slog.Info("Updating TTLSecondsAfterFinished", "namespace", job.Namespace, "name", job.Name)
-
-			if _, err := a.k8s.BatchV1().Jobs(job.Namespace).Patch(ctx, job.Name, types.MergePatchType, a.payload, v1.PatchOptions{}); err != nil {
-				slog.Error("patch job", "err", err, "namespace", job.Namespace, "name", job.Name)
-			}
+	concurrent.ChanUntilDone(ctx, watcher.ResultChan(), func(event watch.Event) {
+		job, ok := event.Object.(*batchv1.Job)
+		if !ok {
+			return
 		}
-	}
+
+		if job.Spec.TTLSecondsAfterFinished != nil && *job.Spec.TTLSecondsAfterFinished != 0 {
+			return
+		}
+
+		if job.Status.Succeeded != 1 {
+			return
+		}
+
+		slog.Info("Updating TTLSecondsAfterFinished", "namespace", job.Namespace, "name", job.Name)
+
+		if _, err := s.k8s.BatchV1().Jobs(job.Namespace).Patch(ctx, job.Name, types.MergePatchType, s.payload, v1.PatchOptions{}); err != nil {
+			slog.Error("patch job", "err", err, "namespace", job.Namespace, "name", job.Name)
+		}
+	}, func() {
+		watcher.Stop()
+		done = true
+	})
+
+	return done
 }
