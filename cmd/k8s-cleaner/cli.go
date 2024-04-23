b156c83079862eecee7ba17ff313123897d357c2
Vincent Boutour <bob@vibioh.fr>
2024-04-23 23:05:46 +0200

    refactor: Removing useless errcheck for parse flag
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/k8s-cleaner/cli.go
+++ cmd/k8s-cleaner/cli.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
@@ -33,9 +32,7 @@ func main() {
 	k8sConfig := k8s.Flags(fs, "k8s")
 	jobConfig := job.Flags(fs, "job")
 
-	if err := fs.Parse(os.Args[1:]); err != nil {
-		log.Fatal(err)
-	}
+	_ = fs.Parse(os.Args[1:])
 
 	alcotest.DoAndExit(alcotestConfig)
 
